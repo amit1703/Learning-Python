ae06d750e993dff130e83f3177a5debecfaf0cb4
amit1703 <amit.izhari@gmail.com>
2022-03-07 18:42:15 +0200

    for loops
    added some basic code of for loops

---
+++ learningpy.py
@@ -0,0 +1,15 @@
+#for loops
+
+
+name = "amit"
+for letter in name:
+    print(letter)
+print("\n")
+
+num = len(name)#giving the length of the string
+
+for i in range(0,num):#running for 0 to num(the length of the string)
+    print(i)
+print("\n")
+
+
