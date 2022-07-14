541544888675dc3acf2a4dc4964d33aeec3e0dd2
amit1703 <83718363+amit1703@users.noreply.github.com>
2022-07-14 17:53:53 +0300

    shutil and os
    add some new info about the built in functions shutil and os

--- shutil_os.py
+++ shutil_os.py
@@ -1,1 +1,13 @@
-print('hello')
+import os
+import shutil
+import send2trash
+f = open('practice.txt','w+')#if the file dont exist, open a new file in the name, if exist, opens the file
+f.write('this is a test string')
+f.close()
+os.getcwd()#diractory location  
+os.listdir()#show all files in this diractory
+os.listdir('c:\\Users') #giving back item in c:\\Users
+shutil.move('practice.txt', 'Desktop')#moves src(practice.txt) to a diffrent location (desktop)
+shutil.move('practice.txt', 'os.getcwd')#moves src(practice.txt) to this diractory
+send2trash.send2trash('practice.txt')#throws the file to trash(not complitly deleting it)
+os.walk
