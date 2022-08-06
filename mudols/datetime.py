0afc357836b058349e300090749fbbc221de389f
amit1703 <amit.izhari@gmail.com>
2022-08-06 17:02:30 +0300

    date time modul
    opened a new folder for all the moduls and added some new info on date time modul

---
+++ mudols/datetime.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+from datetime import datetime
+
+mytime = datetime.time(16,34,12)#hours,mintues,seconds,miliseconds,tzinfo
+print(mytime)#16:34:12
+today = datetime.date.today()# year,month,day of today 
+print(today)#current year month and day
+mydatetime = datetime(2022,8,5,16,12,5)
+
+print(mydatetime)#2022-8-5 16:12:05
