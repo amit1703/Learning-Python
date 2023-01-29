5fdf188e9ba781583d014bd6368d08c6f04da4a7
amit1703 <amit.izhari@gmail.com>
2023-01-29 16:25:05 +0200

    tick tack toe fix
    fixed mile stone project 1

--- advanced/collections.py
+++ advanced/collections.py
@@ -1,6 +1,6 @@
 from __future__ import barry_as_FLUFL
 from collections import Counter, defaultdict, namedtuple
-mylist = [1,1,1,3,1,1,1,1,1,2,2,2,2,2,2,1,1,1,2,2,3,3,3,3,3,3]
+mylist = [1,1,1,3,1,1,1,1,1,2,2,2,2,2,pyto2,1,1,1,2,2,3,3,3,3,3,3]
 Counter(mylist)
 sentenace = "how are you doing today sir, good to hear that"
 letter = 'aaaaaaaaaaaaaaaaaccccccccccccbbbbbbbbbbbdddddddd'
@@ -12,5 +12,6 @@ d =  defaultdict(lambda: 0)
 mytuple = (10,20,30)
 dog  = namedtuple( 'Dog',['age','bread','name'])
 tahel  = dog(age = 5, bread= 'puncher', name= ' tahel')
+
 
 
