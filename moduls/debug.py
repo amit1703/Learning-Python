f9885a27f248dd6127d57049010cf41e006d7485
amit1703 <83718363+amit1703@users.noreply.github.com>
2023-01-30 21:33:44 +0200

    added some info abt  random and math libs

---
+++ moduls/debug.py
@@ -0,0 +1,12 @@
+import pdb
+
+x = [1, 2, 3]
+y = 2
+z = 3
+result_one = y + z
+# added a text box to debug mid opp
+# to try dind by your own your error(pause in the middle of the script)
+pdb.set_trace()
+
+result_two = y + x
+pattern = "phone"
---
+++ moduls/math_random.py
@@ -0,0 +1,10 @@
+import math
+import random
+
+
+list = []
+i = 5.5234242
+print(math.pi)
+for i in range(1, 10):
+    list.append(random.randint(0, 100))
+print(list)
